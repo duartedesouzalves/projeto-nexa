e14eb16ace2676939b562e9de743981934d26927
Júlia Duarte <juliaduartealves08@gmail.com>
2025-11-14 20:14:51 +0000

    controllers

---
+++ src/controllers/Paginas.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const { join } = require("path");
+const rotaPaginas = Router();
+
+rotaPaginas.get("/cadastro", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "cadastro.html"));
+});
+
+rotaPaginas.get("/login", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "in.html"));
+});
+
+rotaPaginas.get("/personalizar", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "per.html"));
+});
+
+rotaPaginas.get("/questionario", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "quest.html"));
+});
+
+rotaPaginas.get("/tema", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "tema.html"));
+});
+
+rotaPaginas.get("/chat", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "chat.html"));
+});
+
+rotaPaginas.get("/contatos", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "contatos.html"));
+});
+
+rotaPaginas.get("/configuracoes", async (req, res) => {
+  res.sendFile(join(__dirname, "..", "..", "pages", "conf.html"));
+});
+
+module.exports = { rotaPaginas };
