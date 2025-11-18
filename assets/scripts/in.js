9b320b891c8e00096561d1aa3c400ff0ccebedce
Júlia Duarte <juliaduartealves08@gmail.com>
2025-11-18 20:24:22 +0000

    scripts

--- assets/scripts/in.js
+++ assets/scripts/in.js
@@ -20,3 +20,38 @@ localStorage.setItem(
 
 // pega
 const dados = JSON.parse(localStorage.getItem("formulario"));
+
+API_URL = "https://ltrk5t-3000.csb.app/";
+const form = document.querySelector("form");
+form.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const productGmail = document.getElementById("gmail").value;
+  const productNome = document.getElementById("nome").value;
+  const productSenha = document.getElementById("senha").value;
+
+  const postData = {
+    productGmail: productGmail,
+    price: productPrice,
+    imageUrl: "teste",
+    description: productSenha,
+  };
+
+  try {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    };
+    const response = await fetch(API_URL, options);
+    if (!response.ok) {
+      throw new Error(`Erro: ${response.status}`);
+    }
+    const dados = await response.json();
+    location.reload();
+  } catch (error) {
+    console.error("Erro na requisição:", error.message);
+  }
+});
