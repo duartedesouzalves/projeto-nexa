86cf199059ddae84ff7e445e755aef0e900da9f9
Júlia Duarte <juliaduartealves08@gmail.com>
2025-11-19 20:28:51 +0000

    scripts

--- assets/scripts/quest.js
+++ assets/scripts/quest.js
@@ -1,10 +1,23 @@
 const dados = JSON.parse(localStorage.getItem("formulario"));
 
-function proximo() {
+async function proximo() {
   dados.resposta = document.querySelector("#resposta").value;
 
   // salva
   localStorage.setItem("formulario", JSON.stringify(dados));
+
+  const resposta = await fetch("/api/quest", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dados),
+  });
+
+  if (!resposta.ok) {
+    alert("erro");
+    return;
+  }
 
   window.location.href = "/questionario";
 }
