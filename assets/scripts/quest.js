91636ec800f40fc14fe17ccd5942af4352adc751
Júlia Duarte <juliaduartealves08@gmail.com>
2025-11-20 22:18:52 +0000

    scripts

--- assets/scripts/quest.js
+++ assets/scripts/quest.js
@@ -1,12 +1,17 @@
 const dados = JSON.parse(localStorage.getItem("formulario"));
 
-async function proximo() {
-  dados.resposta = document.querySelector("#resposta").value;
+async function Entre() {
+  dados.insegurancas = document.querySelector("#insegurancas").value;
+  dados.autoestima = document.querySelector("#autoestima").value;
+  dados.confianca = document.querySelector("#confianca").value;
+  dados.medos = document.querySelector("#medos").value;
+  dados.avaliacao = document.querySelector("#avaliacao").value;
+  dados.mudancas = document.querySelector("#mudancas").value;
 
   // salva
   localStorage.setItem("formulario", JSON.stringify(dados));
 
-  const resposta = await fetch("/api/quest", {
+  const resposta = await fetch("/questionarios", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,3 +26,22 @@ async function proximo() {
 
   window.location.href = "/questionario";
 }
+
+
+
+
+// function Entre() {
+//   // salva
+//   localStorage.setItem(
+//     "formulario",
+//     JSON.stringify({
+//       senha: document.querySelector("#resposta").value;
+//     })
+//   );
+
+//   window.location.href = "/questionario";
+// }
+
+// // pega
+// const dados = JSON.parse(localStorage.getItem("formulario"));
+
